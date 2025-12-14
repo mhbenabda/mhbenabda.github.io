c33d7f7246a6d9ea96afd57eed79d780ab59973f
mhbenabda <168547187+mhbenabda@users.noreply.github.com>
2025-12-14 20:01:03 +0000

    Deploying to gh-pages from @ mhbenabda/mhbenabda.github.io@07e6fa3cb1918d5d27ff870330849b4b104777aa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,17 +11,10 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Last update December 2025",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "post-a-post-with-plotly-js",
         
