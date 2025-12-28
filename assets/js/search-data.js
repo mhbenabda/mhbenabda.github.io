82f70913c6aba1f3196f6de54fa5ad38bb13879e
mhbenabda <168547187+mhbenabda@users.noreply.github.com>
2025-12-28 21:38:08 +0000

    Deploying to gh-pages from @ mhbenabda/mhbenabda.github.io@d737aee51d2e877fd5fb720ff69888ca66536b17 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -402,9 +402,9 @@ ninja.data = [{
           section: "News",},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
-          section: "News",},{id: "projects-deep-infrared-wearables-for-continuous-blood-pressure-monitoring",
-          title: 'Deep-Infrared Wearables for Continuous Blood Pressure Monitoring',
-          description: "Master Thesis",
+          section: "News",},{id: "projects-deepview",
+          title: 'DeepView',
+          description: "Master Thesis: Deep-Infrared Wearable for Continuous Blood Pressure Monitoring",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-coughnet",
